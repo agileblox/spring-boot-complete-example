17e1129deba5bc48832536984a874254ec153dfc
neto_m <neto_macedo.20@gmail.com>
2016-12-29 22:22:35 -0300

    implementação de segurança minima

---
+++ src/main/java/com/example/controller/SecurityController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SecurityController {
+		
+	@RequestMapping("/login")
+	public String login(@AuthenticationPrincipal User user, Model model) {
+		if (user != null) {
+			return "redirect:/titulos";
+		}
+		
+		return "Login";
+	}
+	
+	
+	
+	
+
+}
